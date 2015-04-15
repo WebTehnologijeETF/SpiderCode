065de466ad5bbd5f019e0c86382d926d5b68e752
SumejjaPorca <sumejja93@gmail.com>
2015-04-15 23:59:33 +0200

    Editor color changed

--- app/EditorsCtrl.js
+++ app/EditorsCtrl.js
@@ -4,7 +4,7 @@
 	function EditorsCtrl($scope){
 
 		var editor = ace.edit("editor");
-		editor.setTheme("ace/theme/monokai");
+		editor.setTheme("ace/theme/tomorrow_night");
     	editor.getSession().setMode("ace/mode/javascript");
 	}
 
